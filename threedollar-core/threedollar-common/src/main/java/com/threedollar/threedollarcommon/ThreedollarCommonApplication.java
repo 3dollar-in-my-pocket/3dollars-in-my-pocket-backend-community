9311644ebb32194e43e482fd493572471fcccf49
yerimkoko <gochi97@naver.com>
2023-06-22 22:23:21 +0900

    [TDC-2] BaseEntity 설정

---
+++ threedollar-core/threedollar-common/src/main/java/com/threedollar/threedollarcommon/ThreedollarCommonApplication.java
@@ -0,0 +1,13 @@
+package com.threedollar.community.threedollarcommon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThreedollarCommonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThreedollarCommonApplication.class, args);
+	}
+
+}
