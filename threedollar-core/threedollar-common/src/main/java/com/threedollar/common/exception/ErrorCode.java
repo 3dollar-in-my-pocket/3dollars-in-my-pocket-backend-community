064818f7998d5a1519e678179fde43571d2bf936
yerimkoko <gochi97@naver.com>
2023-06-22 23:47:35 +0900

    [TDC-3] exception 설정

---
+++ threedollar-core/threedollar-common/src/main/java/com/threedollar/common/exception/ErrorCode.java
@@ -0,0 +1,5 @@
+package com.threedollar.common.exception;
+
+public enum ErrorCode {
+
+}
---
+++ threedollar-core/threedollar-common/src/main/java/com/threedollar/common/exception/model/BaseException.java
@@ -0,0 +1,23 @@
+package com.threedollar.common.exception.model;
+
+import com.threedollar.common.exception.ErrorCode;
+import lombok.Getter;
+
+
+@Getter
+public abstract class BaseException extends RuntimeException{
+
+    private final ErrorCode errorCode;
+
+    protected BaseException(String message, ErrorCode errorCode) {
+        super(message);
+        this.errorCode = errorCode;
+    }
+
+    protected BaseException(String message, ErrorCode errorCode, Throwable cause) {
+        super(message, cause);
+        this.errorCode = errorCode;
+    }
+
+
+}
