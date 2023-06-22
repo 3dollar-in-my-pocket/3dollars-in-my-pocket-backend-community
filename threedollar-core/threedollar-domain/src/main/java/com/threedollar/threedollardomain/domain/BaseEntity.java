76e9520e0b5c14b82fe38213a561130902362e29
yerimkoko <gochi97@naver.com>
2023-06-22 22:17:05 +0900

    [TDC-2] BaseEntity 설정

--- threedollar-core/threedollar-domain/src/main/java/com/threedollar/threedollardomain/domain/BaseEntity.java
+++ threedollar-core/threedollar-domain/src/main/java/com/threedollar/threedollardomain/domain/BaseEntity.java
@@ -1,15 +1,23 @@
 package com.threedollar.threedollardomain.domain;
 
+import lombok.Getter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import javax.persistence.Column;
+import javax.persistence.EntityListeners;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
 import java.time.LocalDateTime;
 
-public class BaseEntity {
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class BaseEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -18,5 +26,10 @@ public class BaseEntity {
     @CreatedDate
     @Column(nullable = false, updatable = false)
     private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(nullable = false)
+    private LocalDateTime updatedAt;
+
 
 }
--- threedollar-core/threedollar-domain/src/main/java/com/threedollar/threedollardomain/domain/Report.java
+++ threedollar-core/threedollar-domain/src/main/java/com/threedollar/threedollardomain/domain/Report.java
@@ -1,9 +1,16 @@
 package com.threedollar.threedollardomain.domain;
 
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
 import javax.persistence.Entity;
 
 @Entity
-public class Report {
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+
+public class Report extends BaseEntity {
 
 
 
