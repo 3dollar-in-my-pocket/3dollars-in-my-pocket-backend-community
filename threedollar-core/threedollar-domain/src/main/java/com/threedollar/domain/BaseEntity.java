f7a51a99d605e18d8e26137084c78226aca57f7d
yerimkoko <gochi97@naver.com>
2024-01-05 16:20:03 +0900

    [TDC-31] swagger 설정

--- threedollar-core/threedollar-domain/src/main/java/com/threedollar/domain/BaseEntity.java
+++ threedollar-core/threedollar-domain/src/main/java/com/threedollar/domain/BaseEntity.java
@@ -4,10 +4,10 @@ import jakarta.persistence.Column;
 import jakarta.persistence.EntityListeners;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.MappedSuperclass;
 import lombok.Getter;
 import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.Id;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
