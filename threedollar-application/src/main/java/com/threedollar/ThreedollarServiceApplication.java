1a887a9342107918f1c90221b22221b0068b6e01
yerimkoko <gochi97@naver.com>
2024-02-19 22:57:59 +0900

    [TDC-58] sticker cache

--- threedollar-application/src/main/java/com/threedollar/ThreedollarServiceApplication.java
+++ threedollar-application/src/main/java/com/threedollar/ThreedollarServiceApplication.java
@@ -2,9 +2,11 @@ package com.threedollar;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @EnableWebMvc
+@EnableCaching
 @SpringBootApplication(scanBasePackages = {"com.threedollar"})
 public class ThreedollarServiceApplication {
 
