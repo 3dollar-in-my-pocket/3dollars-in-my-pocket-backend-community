a6efda8ae7f95ed03a3d5aa17052f81704749c07
yerimkoko <gochi97@naver.com>
2024-04-24 23:59:21 +0900

    [TDC-85] 사용하지 않는 메소드 제거

--- threedollar-application/src/main/java/com/threedollar/service/poll/PollService.java
+++ threedollar-application/src/main/java/com/threedollar/service/poll/PollService.java
@@ -1,16 +1,12 @@
 package com.threedollar.service.poll;
 
 import com.threedollar.domain.poll.Poll;
-import com.threedollar.domain.poll.PollCategory;
 import com.threedollar.domain.poll.repository.PollRepository;
 import com.threedollar.service.poll.dto.request.PollCreateRequest;
-import com.threedollar.service.poll.dto.response.AllPollResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.List;
-import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -28,30 +24,6 @@ public class PollService {
     public void deletePoll(Long pollId, String accountId, String targetId, String workspaceId) {
         Poll poll = PollServiceHelper.getPollByIdAndAccountTypeAndWorkspaceId(pollRepository, pollId, accountId, targetId, workspaceId);
         poll.delete();
-    }
-
-
-
-    @Transactional(readOnly = true)
-    public List<AllPollResponse> getAllPollResponse(Long cursor, int size, PollCategory pollCategory) {
-        int checkSize = size + 1;
-
-        List<Poll> pollList = pollRepository.findAllPollList(cursor, checkSize, pollCategory);
-        if (pollList.size() == checkSize) {
-
-        }
-
-        pollList.get(size);
-
-
-        boolean validateEmpty = validateEmpty(pollList);
-        return pollList.stream()
-                .map(AllPollResponse::of)
-                .collect(Collectors.toList());
-    }
-
-    private boolean validateEmpty(List<Poll> pollList) {
-        return !pollList.isEmpty();
     }
 
 
