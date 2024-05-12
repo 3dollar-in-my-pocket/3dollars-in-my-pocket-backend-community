9d669b8f618b69269a94c82c8aa79d6366d679d9
yerimkoko <gochi97@naver.com>
2024-05-12 15:04:31 +0900

    [TDC-86] application.yml 설정

--- threedollar-application/src/main/java/com/threedollar/config/WebConfig.java
+++ threedollar-application/src/main/java/com/threedollar/config/WebConfig.java
@@ -22,7 +22,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(apiKeyHandleInterceptor)
-            .excludePathPatterns("/ping");
+            .excludePathPatterns("/ping", "/health/*");
     }
 
     @Override
