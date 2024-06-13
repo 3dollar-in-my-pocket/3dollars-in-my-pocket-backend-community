4d8a86835a6a86f6c066ff98ecf75ef329932523
yerimkoko <gochi97@naver.com>
2024-06-13 16:05:22 +0900

    [TDC-97] Post Domain 설계

--- threedollar-core/threedollar-domain/src/main/java/com/threedollar/domain/post/postcategory/PostCategory.java
+++ threedollar-core/threedollar-domain/src/main/java/com/threedollar/domain/post/postcategory/PostCategory.java
@@ -1,9 +1,14 @@
-package com.threedollar.domain.post;
+package com.threedollar.domain.post.postcategory;
 
 import com.threedollar.domain.BaseEntity;
+import com.threedollar.domain.post.Post;
+import com.threedollar.domain.post.PostGroup;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
@@ -18,6 +23,10 @@ public class PostCategory extends BaseEntity {
 
     @NotBlank
     private String workspaceId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "post_id", nullable = false)
+    private Post post;
 
     @NotNull
     @Enumerated(EnumType.STRING)
--- threedollar-core/threedollar-domain/src/main/java/com/threedollar/domain/post/postsection/PostSection.java
+++ threedollar-core/threedollar-domain/src/main/java/com/threedollar/domain/post/postsection/PostSection.java
@@ -1,10 +1,15 @@
-package com.threedollar.domain.post;
+package com.threedollar.domain.post.postsection;
 
 import com.threedollar.domain.BaseEntity;
+import com.threedollar.domain.post.Post;
+import com.threedollar.domain.post.PostType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -19,6 +24,10 @@ public class PostSection extends BaseEntity {
     @Enumerated(EnumType.STRING)
     @Column(nullable = false, length = 200)
     private PostType postType;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "post_id", nullable = false)
+    private Post post;
 
     @Column(length = 200)
     private Long parentId;
